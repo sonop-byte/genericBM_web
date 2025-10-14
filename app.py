de8131f083c34730c90031dadecaf4a807c859d8
sonop-byte <okada@medicmedia.com>
2025-10-14 14:11:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -308,8 +308,7 @@ with tab_two:
         st.session_state.run_two = False
 
 # ▼ 1対1：生成済み一覧・DL・複数プレビュー
-- if st.session_state.results_two:
-+ if (not st.session_state.run_two) and st.session_state.results_two:
+if (not st.session_state.run_two) and st.session_state.results_two:
     st.subheader("📄 生成済み差分PDF")
     st.caption("クリックでプレビュー表示（複数可）")
 
@@ -412,8 +411,7 @@ with tab_three:
         st.session_state.run_three = False
 
 # ▼ 1対2：生成済み一覧・DL・複数プレビュー
-- if st.session_state.results_three:
-+ if (not st.session_state.run_three) and st.session_state.results_three:
+if (not st.session_state.run_three) and st.session_state.results_three:
     st.subheader("📄 生成済み差分PDF")
     st.caption("クリックでプレビュー表示（複数可）")
 
